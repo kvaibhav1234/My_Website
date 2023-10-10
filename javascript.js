8d4e6607f57a201c467653cad4f71550011ddeb6
Kashish Vaibhav <64479356+kvaibhav1234@users.noreply.github.com>
2023-10-10 19:17:22 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+<script>
+    document.write("Current Data & Time: ");  
+    now = new Date(); 
+    localtime = now.toString(); 
+    utctime = now.toGMTString(); 
+    document.write("<p><strong> Local time: </strong> + localtime + </p>");
+    document.write("<p><strong> UTC time: </strong> + utctime + </p>");
+
+    </script> 
